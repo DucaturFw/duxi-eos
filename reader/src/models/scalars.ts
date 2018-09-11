879c21b682d07174884d559305f9bf3e2b31bc24
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 18:52:17 +0300

    fix(reader): Now, `DateString` is alias to `Date`

--- reader/src/models/scalars.ts
+++ reader/src/models/scalars.ts
@@ -1,7 +1,7 @@
 export type Hash = string;
 export type Name = string;
 export type AccountName = Name;
-export type DateString = string;
+export type DateString = Date;
 export type Signature = string;
 export type HexData = string;
 export type Address = string;
