5f084cf7b441b449a53a2575d017dae756b7dc4e
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 12:02:23 +0300

    feat(reader): `IContext` inteface to store state of app

---
+++ reader/src/context.ts
@@ -0,0 +1,18 @@
+import r from 'rethinkdb';
+
+export interface IEosWatchOptions {
+  app: string;
+  delay: number;
+  rethinkHost: string;
+  rethinkPort: number;
+  rethinkDatabase: string;
+  rethinkTable: string;
+}
+
+export interface IEosOptions {}
+
+export interface IContext {
+  options: IEosWatchOptions;
+  db: r.Db;
+  conn: r.Connection;
+}
