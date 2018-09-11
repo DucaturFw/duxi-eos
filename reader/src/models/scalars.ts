318f60e4705fa75332f03a5e0e04163350d4105b
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 11:49:10 +0300

    feat(Helpful aliases to base types):

---
+++ reader/src/models/scalars.ts
@@ -0,0 +1,8 @@
+export type Hash = string;
+export type Name = string;
+export type AccountName = Name;
+export type DateString = string;
+export type Signature = string;
+export type HexData = string;
+export type Address = string;
+export type TransactionState = 'executed';
