bad6deab7d4e8c4ac15f1a595393720a006ab6a7
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 13:28:03 +0300

    refactor(reader): Refactor rethinkdb helpers
    Makes rethinkdb helpers more functional like

--- reader/src/database/utils.ts
+++ reader/src/database/utils.ts
@@ -1,14 +1,10 @@
-import r from 'rethinkdb';
+import r, { table } from 'rethinkdb';
 import { IContext } from '../context';
-import { important } from '../utils';
+import { important, DeepPartial } from '../utils';
 
-export const getConnection = (ctx: {
-  options?: {
-    rethinkHost?: string;
-    rethinkPort?: number;
-  };
-  conn?: r.Connection;
-}) => async (): Promise<r.Connection> => {
+export const getConnection = () => async (
+  ctx: Partial<IContext>
+): Promise<r.Connection> => {
   if (ctx.conn && ctx.conn.open) {
     return ctx.conn;
   } else {
@@ -19,10 +15,9 @@ export const getConnection = (ctx: {
   }
 };
 
-export const getOrCreateDatabase = (ctx: {
-  conn?: r.Connection;
-  db?: r.Db;
-}) => async (database: string): Promise<r.Db> => {
+export const getOrCreateDatabase = (database: string) => async (
+  ctx: Partial<IContext>
+): Promise<r.Db> => {
   if (ctx.db) {
     return ctx.db;
   }
@@ -35,10 +30,10 @@ export const getOrCreateDatabase = (ctx: {
   return r.db(database);
 };
 
-export const getOrCreateTable = (ctx: Partial<IContext>) => async (
+export const checkOrCreateTable = (
   table: string,
   opts?: r.TableOptions
-): Promise<r.Table> => {
+) => async (ctx: Partial<IContext>): Promise<r.Table> => {
   if (ctx.tables && ctx.tables[table]) {
     return ctx.tables[table];
   }
@@ -51,4 +46,26 @@ export const getOrCreateTable = (ctx: Partial<IContext>) => async (
   }
 
   return database.table(table);
+};
+
+export const checkOrCreateIndex = <U>(
+  tableName: string,
+  indexName: string,
+  indexFunc?: r.IndexFunction<U>
+) => async (ctx: Partial<IContext>): Promise<string> => {
+  const indexKey = `${tableName}_${indexName}`;
+  if (ctx.indexes && ctx.indexes[indexKey]) {
+    return indexKey;
+  }
+
+  const connection = important(ctx.conn);
+  const table = important(ctx.tables[tableName]);
+
+  let indexes = await table.indexList().run(connection);
+  if (indexes.indexOf(indexName) == -1) {
+    await table.indexCreate(indexName, indexFunc).run(connection);
+    await table.indexWait(indexName).run(connection);
+  }
+
+  return indexKey;
 };
