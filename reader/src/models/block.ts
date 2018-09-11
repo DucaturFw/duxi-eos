963a8ddbb3183f29c5393587aa3356e11378670c
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 11:48:13 +0300

    feat(Add typegoose model of Block record):

---
+++ reader/src/models/block.ts
@@ -0,0 +1,44 @@
+import { prop, Typegoose } from 'typegoose';
+import {
+  Hash,
+  Name,
+  AccountName,
+  DateString,
+  Signature,
+  HexData,
+  Address,
+  TransactionState
+} from './scalars';
+
+export class Block extends Typegoose {
+  @prop({ required: true })
+  block_id: Hash;
+  @prop({ required: true })
+  block: {
+    timestamp: DateString;
+    producer: AccountName;
+    confirmed: number;
+    previous: Hash;
+    transaction_mroot: Hash;
+    action_mroot: Hash;
+    schedule_version: 0;
+    new_producers: null;
+    header_extensions: any[];
+    producer_signature: Signature;
+    transactions: any[];
+    block_extensions: any[];
+  };
+
+  @prop({ required: true })
+  block_num: number;
+  @prop({ required: true })
+  createdAt: DateString;
+  @prop({ required: true })
+  in_current_chain: boolean;
+  @prop({ required: true })
+  irreversible: boolean;
+  @prop({ required: true })
+  updatedAt: DateString;
+  @prop({ required: true })
+  validated: boolean;
+}
