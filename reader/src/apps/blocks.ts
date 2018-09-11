0bd96a4cc126588aac97fdadcbdd2e4c01c73a9a
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 18:54:04 +0300

    refactor(reader): Move implementation of `blocks` app to brand new generator

--- reader/src/apps/blocks.ts
+++ reader/src/apps/blocks.ts
@@ -1,46 +1,15 @@
-import { IContext } from '../context';
-import { getLastSyncedBlock } from '../database';
 import { BlockModel } from '../models/block';
+import { makeReader } from './base-reader';
 
-import mongoose from 'mongoose';
-import { initialize } from './initiate-app';
-
-export async function run(ctx: Partial<IContext>) {
-  let context = { ...ctx } as IContext;
-  try {
-    context.logger.verbose('run blocks');
-    context.tables = {};
-    context.indexes = {};
-    context = await Promise.resolve<Partial<IContext>>(context).then(
-      initialize('blocks')
-    );
-
-    const last = await getLastSyncedBlock(context);
-    const unsyncedBlocks = (await BlockModel.find({
-      block_num: { $gt: last }
-    }).exec())
-      .map(t => t.toObject())
-      .map(t => ((t._id = t._id.toString()), t));
-
-    console.log(`Sync ${unsyncedBlocks.length} blocks`);
-
-    const result = await context.db
-      .table('blocks')
-      .insert(unsyncedBlocks, {
-        conflict: 'error'
-      })
-      .run(context.conn);
-
-    console.log(result);
-  } catch (e) {
-    context.logger.error(e);
-  }
-
-  if (context.conn && context.conn.open) {
-    context.conn.close();
-  }
-
-  if (mongoose.connection && mongoose.connection.readyState != 0) {
-    mongoose.disconnect();
-  }
-}
+export const run = makeReader({
+  app: 'blocks',
+  table: 'blocks',
+  indexes: ['block_num', 'block_id'],
+  model: BlockModel,
+  lastSaved: ctx =>
+    (ctx.tables['blocks'] as any)
+      .max('block_num')('block_num')
+      .default(0)
+      .run(ctx.conn),
+  recent: last => ({ block_num: { $gt: last } })
+});
