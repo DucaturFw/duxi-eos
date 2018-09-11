63bab2f3b1d8bb1307d5ff1663f98bf832670ba4
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 11:48:50 +0300

    feat(Add typegoose model of Transaction record):

---
+++ reader/src/models/transaction.ts
@@ -0,0 +1,51 @@
+import { prop, Typegoose } from 'typegoose';
+import {
+  Hash,
+  Name,
+  AccountName,
+  DateString,
+  Signature,
+  HexData,
+  Address,
+  TransactionState
+} from './scalars';
+
+export class Transaction extends Typegoose {
+  @prop({ required: true })
+  trx_id: Hash;
+  @prop({ required: true })
+  accepted: boolean;
+  @prop({ required: true })
+  actions: Array<{
+    account: AccountName;
+    name: Name;
+    authorization: Array<{ actor: AccountName; permission: string }>;
+    data: HexData;
+  }>;
+  @prop({ required: true })
+  context_free_actions: any[];
+  @prop({ required: true })
+  context_free_data: any[];
+  @prop({ required: true })
+  createdAt: DateString;
+  @prop({ required: true })
+  delay_sec: number;
+  @prop({ required: true })
+  expiration: DateString;
+  @prop({ required: true })
+  implicit: boolean;
+  @prop({ required: true })
+  max_cpu_usage_ms: number;
+  @prop({ required: true })
+  max_net_usage_words: number;
+  @prop({ required: true })
+  ref_block_num: number;
+  @prop({ required: true })
+  ref_block_prefix: string;
+  @prop({ required: true })
+  scheduled: false;
+  @prop({ required: true })
+  signatures: any[];
+  @prop({ required: true })
+  transaction_extensions: any[];
+}
