6b411f9394a259a203a92b77435574335e3dfd72
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 11:50:04 +0300

    feat(reader): Reexport all models from `./models`

---
+++ reader/src/models/index.ts
@@ -0,0 +1,17 @@
+export {
+  Hash,
+  Name,
+  AccountName,
+  DateString,
+  Signature,
+  HexData,
+  Address,
+  TransactionState
+} from './scalars';
+
+export { Account } from './account';
+export { ActionTrace } from './action-trace';
+export { BlockState } from './block-state';
+export { Block } from './block';
+export { TransactionTrace } from './transaction-trace';
+export { Transaction } from './transaction';
