3f1350bc1ac921783a0ce20b22012c6df017a43e
Aler Denisov <aler.zampillo@gmail.com>
2018-09-11 19:47:05 +0300

    fix(reader): Strong typed applications in entrypoint

--- reader/src/index.ts
+++ reader/src/index.ts
@@ -1,33 +1,44 @@
 require('dotenv').config();
-
 import minimist from 'minimist';
+
+import { run as actionTraces } from './apps/action-traces';
+import { run as blockStates } from './apps/block-states';
+import { run as blocks } from './apps/blocks';
+import { run as pubKeys } from './apps/pub-keys';
+import { run as transactionTraces } from './apps/transaction-traces';
+import { run as transactions } from './apps/transactions';
 
 async function run() {
   const { app } = minimist<{
     app?: string;
   }>(process.argv.slice(2));
 
-  let apps: string[] = [
-    'blocks',
-    'transactions',
-    'transaction-traces',
-    'pub-keys',
-    'block-states',
-    'action-traces'
-  ];
+  let appsMap = {
+    blocks: blocks,
+    transactions: transactions,
+    'transaction-traces': transactionTraces,
+    'pub-keys': pubKeys,
+    'block-states': blockStates,
+    'action-traces': actionTraces
+  };
+
+  let apps = [];
+
   const logger = {
-    verbose: console.log,
-    log: console.log,
-    error: console.error
+    verbose: (..._: any[]) => {},
+    log: (..._: any[]) => {},
+    error: (..._: any[]) => {}
   };
 
   if (app) {
     apps = [app];
+  } else {
+    apps = Object.keys(appsMap);
   }
 
   for (let name of apps) {
     logger.verbose('run ' + name);
-    await require(`./apps/${name}`).run({
+    await appsMap[name]({
       app: name,
       logger
     });
